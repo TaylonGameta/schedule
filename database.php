384fc83b38764a8993dfc84732213c70f26c2973
TaylonSopeletto <gtataylon@gmail.com>
2022-05-21 11:28:33 -0300

    database credentials

--- database.php
+++ database.php
@@ -1,11 +1,17 @@
 <?php 
 
+    $user = $_ENV["DB_USER"];
+    $password = $_ENV["DB_PASSWORD"];
+    $host = $_ENV["DB_HOST"];
+    $database = $_ENV["DB_DATABASE"];
+
+
 class Database{
     public $conn;
 
     public function create(){
         try{
-            $this->conn = new PDO('mysql:dbname=schedule;host=127.0.0.1', 'root', '');
+            $this->conn = new PDO("mysql:dbname=$database;host=$host", $user, $password);
         }catch(PDOException $e){
             echo $e->getMessage();
         }
